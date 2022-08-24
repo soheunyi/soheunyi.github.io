2a61f3314db751b10231d9d6250c9ae813c48be5
lsdluis1 <lsdluis1@gmail.com>
2022-08-25 02:07:20 +0900

    Update google scholar icon

---
+++ docs/link_icon.js
@@ -0,0 +1,7 @@
+var element = document.createElement("link");
+element.setAttribute("rel", "stylesheet");
+element.setAttribute(
+  "href",
+  "https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css"
+);
+document.getElementsByTagName("head")[0].appendChild(element);
